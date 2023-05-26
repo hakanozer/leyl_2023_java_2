c6994eaf08d5d8f9a6dc2a8f356447c1d9daf79f
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-26 10:57:47 +0300

    Update UseMainThread.java

--- Days-4/src/main/java/useThread/UseMainThread.java
+++ Days-4/src/main/java/useThread/UseMainThread.java
@@ -3,19 +3,25 @@ package useThread;
 public class UseMainThread {
     public static void main(String[] args) {
 
-        Action action1 = new Action("image-1.jpg");
-        Thread th1 = new Thread(action1);
-        th1.start();
+        Runnable rn = () -> {
+            try {
+                Action action1 = new Action("image-1.jpg");
+                Thread th1 = new Thread(action1);
+                th1.start();
+                th1.join();
 
-        Action action2 = new Action("image-2.jpg");
-        Thread th2 = new Thread(action2);
-        th2.start();
+                Action action2 = new Action("image-2.jpg");
+                Thread th2 = new Thread(action2);
+                th2.start();
+                th2.join();
+            }catch (Exception ex) {}
+        };
+        new Thread(rn).start();
 
         int a = 10;
         int b = 20;
         int sum = a + b;
         System.out.println("Sum : " + sum);
-
 
     }
 }
