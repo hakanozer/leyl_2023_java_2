48f05e1131e44f79024133e086652b9c96dd5e3f
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-27 11:08:37 +0300

    Config Class

---
+++ Days-5/src/main/java/configs/AppConfig.java
@@ -0,0 +1,10 @@
+package configs;
+
+public class AppConfig {
+
+    public final static int rowCount = 50;
+    public final static String apiKey = "FGJFGGSF33453dfgdfgDFGd";
+    public final static String theme = "dark";
+
+
+}
---
+++ Days-5/src/main/java/configs/MainConfig.java
@@ -0,0 +1,10 @@
+package configs;
+
+public class MainConfig {
+    public static void main(String[] args) {
+
+        //AppConfig.apiKey = "";
+        System.out.println( AppConfig.apiKey );
+
+    }
+}
